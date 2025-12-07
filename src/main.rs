1772ba2ebc2bd9dae6de1bfaf549e53ae7a3f45b
mirkodonnarumma-rs <mirko.donnarumma@outlook.it>
2025-12-07 17:44:15 +0100

    feat: add .env.toml and dry-run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
+use toml::{Value, from_str};
+
 fn main() {
-    println!("Hello, world!");
+    // load file as a string
+    let raw = fs::read_to_string("./.env.toml")
+    .expect("Cannot read ./.env.toml");
+    // copy it inside a toml formatted value
+    let config: Value = from_str(&raw)
+    .expect("Invalid TOML");
+
+    println!("{:#?}", config);
 }
